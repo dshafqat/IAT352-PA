ffbb3194f2ca3db7c726f5ef2bba546c535da26f
Dawood <dshafqat@sfu.ca>
2020-10-07 11:42:37 -0700

    Form completed

--- sign-up.php
+++ sign-up.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
+
 <html lang="en">
 
 <head>
 	<meta charset="utf-8">
-	<title>Menu</title>
+
+	<title>Sign Up</title>
 
 
 	<link rel="stylesheet" href="css/main.css"> 
@@ -33,7 +35,7 @@
 	</header>
 
 
-	<div class="signup-form">
+	<!-- <div class="signup-form">
 		<h1>Sign up</h1>
 
 		<form class ="signupform" action="registration.php" method="get">
@@ -61,7 +63,90 @@
 
   </form>
 
-</div>
+</div> -->
+
+	<body>
+
+			<div class="form-signup">
+	<form action="sign-up.php" method="post">
+		<fieldset>
+			<legend><span class="number"> Sign Up</legend>
+				<input type="text" name="name"  placeholder="Enter Name *"  value="<?php  $name;?>" >
+
+				<input type="email" name="email" placeholder="Enter Email *" value="<?php  $email;?>">
+				<input type="password" name="password" placeholder="Create a Password *" value="<?php  $password;?>">
+
+				<input type="password" name="RepassWord" placeholder="Re-Enter the passward *" value="<?php  $RepassWord;?>">
+
+
+					<label for="job">Tea Preference:</label>
+				<select id="" name="choice">
+
+
+				  <option value="BubbleTea">Bubble Tea</option>
+				  <option value="MilkTea">Milk Tea</option>
+
+				</select>
+
+				</fieldset>
+
+				<input type="submit" name="submit" value="Submit" />
+				</form>
+				</div>
+
+<!-- Empty variables check-->
+<?php
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	// define variables and set to empty values
+$name = $email = $password = $ReEnterPassword = "";
+
+$name = $_POST["name"];
+$email = $_POST["email"];
+$password = $_POST["password"];
+$ReEnterPassword = $_POST["RepassWord"];
+$choice = $_POST["choice"];
+
+$file = 'memberInfo.txt';
+// Open connection to the $file in writing mode
+
+	// check if form is valid
+	if ($password != $ReEnterPassword) {
+			// If passwords not the same
+			exit("*Passwords missmatch!");
+	}
+
+ 	if ($name == "" || $email == "" || $password == "" || $choice =="")  {
+
+		// If missing fields echo this message
+		exit("*Please complete the form");
+
+	}
+	else if($handle = fopen($file, 'a+')) {
+
+
+	// Write string to the file
+	// the function returns the number of bytes interted or false
+	fwrite($handle, "Name:$name, Email:$email, Password:$password, Choice:$choice\n");
+
+	// After form is completed, redirect to welcome.php to display text otherwise display error
+
+	header("Location:welcome.php");
+    exit;
+
+	fclose($handle);
+	} else {
+	echo "Error, could not open file for writing.";
+	}
+
+}
+
+
+
+?>
+
+
 
 
 
---
+++ welcome.php
@@ -0,0 +1,34 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Welcome</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+
+
+
+<div class="container-fluid">
+
+		<div class="col-md-12" align="center">
+ 
+		 <h1>Congrats! You are now a memeber</h1>
+		 <a href="index.php" class="btn btn-primary btn-lg active" role="button">Return to Menu</a>
+
+		</div>
+
+</div>
+
+
+</body>
+
+</html>
+
+
