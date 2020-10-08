f226a32032f52a1b0834fa691ab81867e50682bd
kimmychu <66520175+kimmychu@users.noreply.github.com>
2020-10-08 09:04:36 +0800

    Update action.php

--- action.php
+++ action.php
@@ -29,21 +29,21 @@
 
 		if($result->num_rows>0){
 			while($row=$result->fetch_assoc()){
-				$output .='<div class="col-md-3 mb-2">
+				$output .='<div class="col-md-3 mb-2 border-light">
 					<div class="card-deck">
 						<div class="card border-secondary">
 							<img src="'. $row['product_image'].'" class="card-img-top">
 							<div class="card-img-overlay">
-								<h6 style="margin-top:260px;"class="text-light bg-info text-center rounded p-1">
+								<h6 style="margin-top:260px;"class="text-light text-center rounded p-1">
 									'. $row['product_name'].' </h6>
 								</div>
 								<div class ="card-body">
-									<h4 class="card-title text-danger">R:'. number_format($row['price_r'],1).' &nbsp;L:'. number_format($row['price_l'],1).'</h4>
+									<h4 class="card-title text-center">R:'. number_format($row['price_r'],1).' &nbsp;L:'. number_format($row['price_l'],1).'</h4>
 									<p>
 										Tea : '. $row['tea'] .'<br>
 										Add_ons : '. $row['add_on'] .'<br>
 									</p>
-									<a href="#" class="btn btn-success btn-block">Add to Cart</a>
+									<a href="#" class="btn cart">Add to Cart</a>
 								</div>
 							</div>
 						</div>
