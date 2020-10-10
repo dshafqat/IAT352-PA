3d2875ba2e8f1f9fb3359e1381057c12a8de5c94
kimmychu <66520175+kimmychu@users.noreply.github.com>
2020-10-10 08:09:32 +0800

    Update index.php

--- index.php
+++ index.php
@@ -50,7 +50,19 @@ require 'config.php';
 			<hr>
 			<h6 class="filtertitle">Tea</h6>
 			<ul class="list-group">
-				
+				<?php
+				$sql="SELECT DISTINCT tea FROM products ORDER BY tea";
+				$result=$conn->query($sql);
+				while($row=$result->fetch_assoc()){
+				?>
+				<li class="list-group-item">
+					<div class="form-check">
+						<label class="form-check-label">
+							<input type="checkbox" class="form-check-input product_check" value="<?= $row['tea']; ?> " id="tea"><?= $row['tea'];?>
+						</label>
+					</div>
+				</li>
+			<?php } ?>
 			</ul>
 
 			<h6 class="filtertitle">Add-ons</h6>
@@ -85,6 +97,7 @@ require 'config.php';
 					</div>
 				</li>
 			<?php } ?>
+
 			</ul>
 
 			<h6 class="filtertitle">Price(Large)</h6>
@@ -181,204 +194,4 @@ require 'config.php';
 	});
 </script>
 	</body>
-
-<!--
-
-
-
-	// The body sections except Top Navigation bar & the Footer 
-	<section class="body_Style">
-		<section class="center_text style_text">
-			<h1>Hot Sales</h1>
-		</section>
-
-		<section class="main_container">
-			<div class="main_manu_box">
-				<p>Product1</p>
-
-				<div class="center_text">
-					<img class="image_product_styling" src="img/bubbleTea/bubble_tea_1.png" alt="product image #1">
-					<h3>Original Milk Tea</h3>
-					<p>R:5.0 L:5.5</p>
-					<a href="product1.html"><p><button class="btn btn-3 btn-3a icon-cart">Add to Cart</button></p></a>
-				</div>
-				 Add to Cart Button learned from W3school "https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_product_card"
-				
-			</div>
-
-			<div class="main_manu_box">
-				<p>Product2</p>
-
-				<div class="center_text">
-					<img class="image_product_styling" src="img/bubbleTea/bubble_tea_2.png" alt="product image #2">
-					<h3>Strawberry Green Tea</h3>
-					<p>R:5.0 L:5.5</p>
-					<a href="product2.html"><p><button class="btn btn-3 btn-3a icon-cart">Add to Cart</button></p></a>
-				</div>
-			</div>
-
-			<div class="main_manu_box">
-				<p>Product3</p>
-
-				<div class="center_text">
-					<img class="image_product_styling" src="img/bubbleTea/bubble_tea_3.png" alt="product image #3">
-					<h3>Hokkaido Green Milk Tea</h3>
-					<p>R:5.0 L:5.5</p>
-					<a href="product3.html"><p><button class="btn btn-3 btn-3a icon-cart">Add to Cart</button></p></a>
-				</div>
-			</div>
-
-			<div class="main_manu_box">
-				<p>Product4</p>
-
-				<div class="center_text">
-					<img class="image_product_styling" src="img/bubbleTea/bubble_tea_4.png" alt="product image #4">
-					<h3>Thai Tea</h3>
-					<p>R:5.0 L:5.5</p>
-					<a href="product4.html"><p><button class="btn btn-3 btn-3a icon-cart">Add to Cart</button></p></a>
-				</div>
-			</div>
-
-			<div class="main_manu_box">
-				<p>Product5</p>
-
-				<div class="center_text">
-					<img class="image_product_styling" src="img/bubbleTea/bubble_tea_5.png" alt="product image #5">
-					<h3>Passion Fruit Green Tea</h3>
-					<p>R:5.0 L:5.5</p>
-					<a href="product5.html"><p><button class="btn btn-3 btn-3a icon-cart">Add to Cart</button></p></a>
-				</div>
-			</div>
-
-			<div class="main_manu_box">
-				<p>Product6</p>
-
-				<div class="center_text">
-					<img class="image_product_styling" src="img/bubbleTea/bubble_tea_6.png" alt="product image #6">
-					<h3>Taro Milk Tea</h3>
-					<p>R:5.0 L:5.5</p>
-					<a href="product6.html"><p><button class="btn btn-3 btn-3a icon-cart">Add to Cart</button></p></a>
-				</div>
-			</div>
-
-			<div class="main_manu_box">
-				<p>Product7</p>
-
-				<div class="center_text">
-					<img class="image_product_styling" src="img/bubbleTea/bubble_tea_7.png" alt="product image #7">
-					<h3>Lychee Black Tea</h3>
-					<p>R:5.0 L:5.5</p>
-					<a href="product7.html"><p><button class="btn btn-3 btn-3a icon-cart">Add to Cart</button></p></a>
-				</div>
-			</div>
-
-			<div class="main_manu_box">
-				<p>Product8</p>
-
-				<div class="center_text">
-					<img class="image_product_styling" src="img/bubbleTea/bubble_tea_8.png" alt="product image #8">
-					<h3>Green Apple Green Tea</h3>
-					<p>R:5.0 L:5.5</p>
-					<a href="product8.html"><p><button class="btn btn-3 btn-3a icon-cart">Add to Cart</button></p></a>
-				</div>
-			</div>
-		</section>   // end of the hot sales section 
-
-		<section class="center_text style_text">
-			<h1>Recommended For You</h1>
-		</section>
-
-		<section class="main_container">
-			<div class="main_manu_box">
-				<p>Product9</p>
-				<div class="center_text">
-					<img src="img/bubbleTea/bubble_tea_9.png" alt="product image #1">
-					<h3>Jasmine Green Tea</h3>
-					<p>R:5.0 L:5.5</p>
-					<a href="product9.html"><p><button class="btn btn-3 btn-3a icon-cart">Add to Cart</button></p></a>
-				</div>
-			</div>
-
-			<div class="main_manu_box">
-				<p>Product10</p>
-				<div class="center_text">
-					<img src="img/bubbleTea/bubble_tea_10.png" alt="product image #1">
-					<h3>Pineapple Green Tea</h3>
-					<p>R:5.0 L:5.5</p>
-					<a href="product10.html"><p><button class="btn btn-3 btn-3a icon-cart">Add to Cart</button></p></a>
-				</div>
-			</div>
-
-			<div class="main_manu_box">
-				<p>Product11</p>
-				<div class="center_text">
-					<img src="img/bubbleTea/bubble_tea_11.png" alt="product image #1">
-					<h3>Coconut Milk Tea</h3>
-					<p>R:5.0 L:5.5</p>
-					<a href="product11.html"><p><button class="btn btn-3 btn-3a icon-cart">Add to Cart</button></p></a>
-				</div>
-			</div>
-
-			<div class="main_manu_box">
-				<p>Product12</p>
-				<div class="center_text">
-					<img src="img/bubbleTea/bubble_tea_12.png" alt="product image #1">
-					<h3>Peach Green Tea</h3>
-					<p>R:5.0 L:5.5</p>
-					<a href="product12.html"><p><button class="btn btn-3 btn-3a icon-cart">Add to Cart</button></p></a>
-				</div>
-			</div>
-
-			<div class="main_manu_box">
-				<p>Product13</p>
-				<div class="center_text">
-					<img src="img/bubbleTea/bubble_tea_13.png" alt="product image #1">
-					<h3>Matcha Milk Tea</h3>
-					<p>R:5.0 L:5.5</p>
-					<a href="product13.html"><p><button class="btn btn-3 btn-3a icon-cart">Add to Cart</button></p></a>
-				</div>
-			</div>
-
-			<div class="main_manu_box">
-				<p>Product14</p>
-				<div class="center_text">
-					<img src="img/bubbleTea/bubble_tea_14.png" alt="product image #1">
-					<h3>Matcha Milk Tea</h3>
-					<p>R:5.0 L:5.5</p>
-					<a href="product14.html"><p><button class="btn btn-3 btn-3a icon-cart">Add to Cart</button></p></a>
-				</div>
-			</div>
-
-			<div class="main_manu_box">
-				<p>Product15</p>
-				<div class="center_text">
-					<img src="img/bubbleTea/bubble_tea_15.png" alt="product image #1">
-					<h3>Rose Milk Tea</h3>
-					<p>R:5.0 L:5.5</p>
-					<a href="product15.html"><p><button class="btn btn-3 btn-3a icon-cart">Add to Cart</button></p></a>
-				</div>
-			</div>
-
-			<div class="main_manu_box">
-				<p>Product16</p>
-				<div class="center_text">
-					<img src="img/bubbleTea/bubble_tea_16.png" alt="product image #1">
-					<h3>Mango Green Tea</h3>
-					<p>R:5.0 L:5.5</p>
-					<a href="product16.html"><p><button class="btn btn-3 btn-3a icon-cart">Add to Cart</button></p></a>
-				</div>
-			</div>
-		</section>   
-
-	</section>  // End of the body section 
-
-	// Footer section 
-	<Footer> 
-		<div class="bottomNav">
-			<p class="bottomNav_p">Bubbleorze</p>
-			<p>@2020 Copyright</p>
-		</div>
-	</Footer>
-</body>	
--->
 </html>
