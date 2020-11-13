c34858687c34fa3bfab1d4feca066a1b6c564648
kimmychu <66520175+kimmychu@users.noreply.github.com>
2020-11-14 03:29:41 +0800

    Add comments and fix the cart page

--- cart.php
+++ cart.php
@@ -46,9 +46,10 @@ require 'config.php';
 <div class="cartform">
 <div id="cartlist">
 
-
+	<!-- Connect to the cart table from the database -->
 <?php
 $sql = $conn->query("SELECT * FROM cart");
+//Count the row for the table, start the loop and print the list if the table is not empty
 $row_cnt = $sql->num_rows;
 if($row_cnt > 0){
     $total_price = 0;
@@ -62,11 +63,13 @@ if($row_cnt > 0){
 		<th>UNIT PRICE</th>
 		<th>ITEMS TOTAL</th>
 	</tr> 
+<!-- Connect the table from the database -->
 	<?php
 		$sql="SELECT * FROM cart";
 		$result=$conn->query($sql);
 		while($row=$result->fetch_assoc()){
 	?>
+<!-- Output the data from the table -->	
 <tr>
 	<td><img src="<?= $row['product_image']; ?>" width="50" height="70" /></td>
 	<td><?= $row['product_name']; ?>
@@ -75,8 +78,8 @@ if($row_cnt > 0){
 			<!--<button onclick="removeitem()" type='submit' class='remove'>Remove Item</button>-->
 		</form>
 	</td>
-<td>
-	<script>
+<td><?= $row['qty']; ?></td>
+<!--	<script>
 		function removeitem() {
 		  var txt;
 		  var r = confirm("Are you sure?");
@@ -88,8 +91,8 @@ if($row_cnt > 0){
 	  } 
 	}
 	</script>
-
-
+-->
+<!--
 <?php
 $code=$row['product_code'];
 	if (isset($_REQUEST['setqty'])) {
@@ -98,8 +101,8 @@ $code=$row['product_code'];
 	$sql2 = "UPDATE cart SET qty = '$setqty' WHERE product_code = '$thiscode'";
 	//$result = $conn->query($sql2);
 }
-?>
-</td>
+?>-->
+<!-- Display the price and calculate the total price-->
 <?php $price=(float)$row['product_price'];?>
 <td><?php echo "$".$price; ?></td>
 <td><?php echo "$".$price*$row['qty']; ?></td>
@@ -126,6 +129,7 @@ $total_price += ($price*$row['qty']);
 </tr>
 
 <div>
+<!-- Switch to other page when submit the cart-->
 <?php
 	if(isset($_POST['submit_button']))
 	{
@@ -136,6 +140,7 @@ $total_price += ($price*$row['qty']);
 	   
 	}
 
+//Clean the table when click on the remove button
 	if(isset($_POST['remove_button']))
 	{
 	    mysqli_query($conn, 'TRUNCATE TABLE `cart`');
@@ -149,6 +154,7 @@ $total_price += ($price*$row['qty']);
   <?php  
 }
 else{
+//Show when the table is empty	
  echo "<h3>Your cart is empty!</h3>";
  }
 ?>
