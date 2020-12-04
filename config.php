599d3c00617d0a850236ba305ceff4fde3481260
kimmychu <66520175+kimmychu@users.noreply.github.com>
2020-12-04 14:45:20 +0800

    Update config.php

--- config.php
+++ config.php
@@ -3,11 +3,11 @@
 $servername="localhost";
 $email="root";
 $password="";
-$dbname="kimmy_Chu";
+$dbname="kimmy_chu";
 $name="";
 
 
- $conn = new mysqli("localhost","root","","kimmy_Chu");
+ $conn = new mysqli("localhost","root","","kimmy_chu");
  if($conn->connect_error){
  	die("Connection Failed!".$conn->connect_error);
  }
